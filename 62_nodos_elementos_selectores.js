9952c5e95618b5da789c78ea8aad71706a49aaf9
LuzAngela2894 <98186763+LuzAngela2894@users.noreply.github.com>
2022-03-07 10:33:29 -0500

    Javascript

---
+++ 62_nodos_elementos_selectores.js
@@ -0,0 +1,15 @@
+//Mozilla Developer Network (MDN) Node.nodeType
+console.log(document.getElementsByTagName("li"));
+console.log(document.getElementsByClassName("card"));
+console.log(document.getElementsByName("nombre"));
+console.log(document.getElementById("menu"));
+console.log(document.querySelector("#menu"));
+console.log(document.querySelector("a"));
+console.log(document.querySelectorAll("a"));
+console.log(document.querySelectorAll("a").length);
+document.querySelectorAll("a").forEach((elemento) => console.log(elemento));
+console.log(document.querySelector(".card"));
+console.log(document.querySelectorAll(".card"));
+console.log(document.querySelectorAll(".card")[2]);
+console.log(document.querySelectorf("#menu li"));
+console.log(document.querySelectorAll("#menu li"));
