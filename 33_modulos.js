c45626540eb08c555810733227fdf59b13d4c2e2
LuzAngela2894 <98186763+LuzAngela2894@users.noreply.github.com>
2022-03-09 18:21:43 -0500

    Javascript

--- 33_modulos.js
+++ 33_modulos.js
@@ -1,6 +1,6 @@
-import saludar, { Saludar, PI, usuario } from "./constantes.js";
-import { aritmetica as calculos} from "./aritmetica.js";
-console.log("Archivos Módulos.js")
+import saludar, { Saludar, PI, usuario } from "./33_constantes.js";
+import { aritmetica as calculos } from "./33_aritmetica.js";
+console.log("Archivos Módulos.js");
 console.log(PI, usuario);
 console.log(calculos.sumar(8, 5));
 console.log(calculos.restar(18, 7));
